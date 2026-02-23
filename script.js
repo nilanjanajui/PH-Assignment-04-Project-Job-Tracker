a2064ee3445ba0729676add9c5c9cf3ee5929833
Nilanjana Jui <nilanjana.csecu@gmail.com>
2026-02-23 18:24:07 +0600

    no available added

---
+++ script.js
@@ -0,0 +1,12 @@
+let interviewList = [];
+let rejectedList = [];
+let currentStatus = 'all';
+
+const total = document.getElementById('total');
+const interviewCount = document.getElementById('interviewCount');
+const rejectedCount = document.getElementById('rejectedCount');
+const jobCount = document.getElementById('jobCount');
+
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
