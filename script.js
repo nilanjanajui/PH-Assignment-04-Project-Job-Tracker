da2740b6aadc15ed6f5ed571e6af8b1a4d6d66d1
Nilanjana Jui <nilanjana.csecu@gmail.com>
2026-02-23 18:28:34 +0600

    card added

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ const jobCount = document.getElementById('jobCount');
 const allFilterBtn = document.getElementById('all-filter-btn');
 const interviewFilterBtn = document.getElementById('interview-filter-btn');
 const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+
+const allCardSection = document.getElementById('allCards');
+const filterSection = document.getElementById('filtered-section');
+const mainContainer = document.querySelector('main');
+const emptyState = document.getElementById('emptyState');
+
+
+function calculateCount() {
+    const totalCards = allCardSection.querySelectorAll('.card').length;
+
+    total.innerText = totalCards;
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+
+    if (currentStatus === 'interview-filter-btn') {
+        jobCount.innerText = `${interviewList.length} of ${totalCards}`;
+    }
+    else if (currentStatus === 'rejected-filter-btn') {
+        jobCount.innerText = `${rejectedList.length} of ${totalCards}`;
+    }
+    else {
+        jobCount.innerText = totalCards;
+    }
+
+    if (totalCards === 0) {
+        emptyState.classList.remove('hidden');
+    } else {
+        emptyState.classList.add('hidden');
+    }
+}
+
+calculateCount();
